686ace9a3e82525896200a407faf21456b19d75d
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-06-30 16:37:12 -0300

    alterei

--- desafio.dart
+++ desafio.dart
@@ -1,16 +1,31 @@
-void main(){
-  int number = 10;
-  String nome = "julia";
-  double dobrado = 11.1;
-  bool vivo = true;
 
-  List <String> lista = ["ju" , "lia"];
- 
-  Set<int> meuset = { 1, 2, 3};
 
-  Map<dynamic,dynamic> meuMap5 = {
-    'idade' : 'nome'
-  };
+void main() {
+  // double idade = 10.9;
+  // idade.round();
+  // print(idade.round());
+
+  // String nome = "julia";
+  
+
+
+  // double dobrado = 11.1;
+  // bool vivo = true;
+
+  // List<String> lista = ['ju', 'lia'];
+  
+  List <String> nomes = ['Julia', 'Guilherme'];
+  List <String> nomesDiferente = ['dllfkdk', 'dkdkd'];
+  nomes.shuffle();
+  nomes.add('dllfkdddddk');
+  nomes.addAll(nomesDiferente);
+  nomes.removeAt(4);
+  print(nomes);
+  
+
+  // Set<int> meuset = {1, 2, 3};
+
+  // Map<dynamic, dynamic> meuMap5 = {'idade': 'nome'};
 
 
 }
