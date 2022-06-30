a50803e2ea5d53a7d0e9fc8cf86b6d31d1ea3ada
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-06-30 15:59:11 -0300

    como escrever variaveis

---
+++ desafio.dart
@@ -0,0 +1,16 @@
+void main(){
+  int number = 10;
+  String nome = "julia";
+  double dobrado = 11.1;
+  bool vivo = true;
+
+  List <String> lista = ["ju" , "lia"];
+ 
+  Set<int> meuset = { 1, 2, 3};
+
+  Map<dynamic,dynamic> meuMap5 = {
+    'idade' : 'nome'
+  };
+
+
+}
